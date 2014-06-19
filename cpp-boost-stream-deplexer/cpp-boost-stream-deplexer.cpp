fe6fc9d126f59ce7b7d9c984929c06eb972b093c
ehevutov <jamesgosnell@gmail.com>
2014-06-19 17:43:18 -0500

    INITIAL commit

---
+++ cpp-boost-stream-deplexer/cpp-boost-stream-deplexer.cpp
@@ -0,0 +1,16 @@
+// cpp-boost-stream-deplexer.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	int x;
+	cout << "Hello, World!" << endl;
+	cin >> x;
+	cout << x << endl;
+	return 0;
+}
+
